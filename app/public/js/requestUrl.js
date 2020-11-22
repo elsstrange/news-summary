98e32e002b07992c6c7385b7f4c0dc0330a5a25d
elsstrange <ellenabstrange@gmail.com>
2020-11-22 08:21:13 +0000

    Adding params for number of sentences to Aylien summary request

--- app/public/js/requestUrl.js
+++ app/public/js/requestUrl.js
@@ -4,7 +4,7 @@ class requestUrl {
   }
 
   createAylienRequest(article) {
-    return `http://news-summary-api.herokuapp.com/aylien?apiRequestUrl=https://api.aylien.com/api/v1/summarize?url=${article}"`
+    return `http://news-summary-api.herokuapp.com/aylien?apiRequestUrl=https://api.aylien.com/api/v1/summarize?url=${article}&sentences_number=7`
   }
 
   formatToday() {
