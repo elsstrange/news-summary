65fde0e9c530c904cfdbeb45ad588b645f7c8215
elsstrange <ellenabstrange@gmail.com>
2020-11-22 18:09:54 +0000

    Implemented summary buttons on index.html Experimenting with promises

--- app/public/js/index.js
+++ app/public/js/index.js
@@ -1,5 +1,28 @@
-let requesturl = new requestUrl().createGuardianRequest('politics')
-let client = new makersClient()
-let targetElement = document.getElementById('headlines')
+let guardianRequestUrl
+let aylienRequestUrl
+let client
+let targetElement
 
-client.get(requesturl, data => new headlineList(data, targetElement).render());
+new Promise((resolve) => {
+  window.addEventListener('load', () => { 
+    setUpPage();
+    resolve();
+  })
+}).then(() => setUpListener())
+
+
+setUpPage = (event) => {
+  guardianRequestUrl = new requestUrl().createGuardianRequest('politics')
+  client = new makersClient()
+  targetElement = document.getElementById('headlines')
+
+  client.get(guardianRequestUrl, data => new headlineList(data, targetElement).render());
+}
+
+setUpListener = () => {
+  window.addEventListener('hashchange', (event) => {
+    articleUrl = `https://www.theguardian.com/${window.location.hash.slice(1)}`
+    aylienRequestUrl = new requestUrl().createAylienRequest(articleUrl)
+    client.get(aylienRequestUrl, data => new articleSummary(data, targetElement).render())
+  })
+}
